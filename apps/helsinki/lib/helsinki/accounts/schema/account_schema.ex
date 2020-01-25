5701ad2a067b953a5ee606aec5d06e1f565af6ff
erik.gonzalez <erik.gonzalez@santiago.mx>
2020-01-24 18:07:19 -0600

    se agrega phx.gen.html para la tabla de Cuentas

---
+++ apps/helsinki/lib/helsinki/accounts/schema/account_schema.ex
@@ -0,0 +1,31 @@
+defmodule AccountingSystem.AccountSchema do
+  use Ecto.Schema
+  import Ecto.Changeset
+
+  schema "accounts" do
+    field :apply_third_party_to, :string
+    field :apply_to, :integer
+    field :character, :boolean, default: false
+    field :code, :string
+    field :group_code, :integer
+    field :is_departamental, :boolean, default: false
+    field :name, :string
+    field :parent_account, :integer
+    field :payment_method, :boolean, default: false
+    field :root_account, :integer
+    field :status, :string
+    field :third_party_op, :boolean, default: false
+    field :third_party_prosecutor, :integer
+    field :type, :string
+    field :uuid_voucher, :string
+
+    timestamps()
+  end
+
+  @doc false
+  def changeset(account, attrs) do
+    account
+    |> cast(attrs, [:code, :status, :type, :name, :is_departamental, :parent_account, :root_account, :character, :group_code, :uuid_voucher, :payment_method, :apply_to, :third_party_prosecutor, :third_party_op, :apply_third_party_to])
+    |> validate_required([:code, :status, :type, :name, :is_departamental, :parent_account, :root_account, :character, :group_code, :uuid_voucher, :payment_method, :apply_to, :third_party_prosecutor, :third_party_op, :apply_third_party_to])
+  end
+end
--- apps/helsinki_web/lib/helsinki_web/router.ex
+++ apps/helsinki_web/lib/helsinki_web/router.ex
@@ -17,6 +17,7 @@ defmodule AccountingSystemWeb.Router do
     pipe_through :browser
 
     get "/", PageController, :index
+    resources "/accounts", AccountController
   end
 
   # Other scopes may use custom stacks.
