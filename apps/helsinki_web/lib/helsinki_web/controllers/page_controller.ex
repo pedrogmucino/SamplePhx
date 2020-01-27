0cc5858e9680fb44320517123f09192ebf2ad3b2
Héctor López <hector.lopez@DSA-DEV2-10>
2020-01-27 16:49:26 -0600

    make and render index

--- apps/helsinki_web/lib/helsinki_web/controllers/page_controller.ex
+++ apps/helsinki_web/lib/helsinki_web/controllers/page_controller.ex
@@ -1,7 +1,8 @@
 defmodule AccountingSystemWeb.PageController do
   use AccountingSystemWeb, :controller
+  alias Phoenix.LiveView
 
   def index(conn, _params) do
-    render(conn, "index.html")
+    LiveView.Controller.live_render(conn, AccountingSystemWeb.PageLiveView, session: %{})
   end
 end
--- apps/helsinki_web/lib/helsinki_web/views/page_view.ex
+++ apps/helsinki_web/lib/helsinki_web/views/page_view.ex
@@ -1,3 +1,18 @@
 defmodule AccountingSystemWeb.PageView do
   use AccountingSystemWeb, :view
 end
+
+defmodule AccountingSystemWeb.PageLiveView do
+
+  use Phoenix.LiveView
+
+  def render(assigns) do
+    Phoenix.View.render(AccountingSystemWeb.PageView, "index.html", assigns)
+  end
+
+  def mount(_params, socket) do
+    {:ok, socket}
+  end
+
+end
+
