cf224fa44df4e594352612e7ca9f4dfc95d79ef5
Héctor López <hector.lopez@DSA-DEV2-10>
2020-02-10 10:11:30 -0600

    open form create new in subacount

--- apps/helsinki_web/lib/helsinki_web/components/sub_accounts_component.ex
+++ apps/helsinki_web/lib/helsinki_web/components/sub_accounts_component.ex
@@ -26,19 +26,19 @@ defmodule AccountingSystemWeb.SubAccountsComponent do
 
           </h1>
           <label class="block text-gray-700 text-sm font-bold">1-1002-1010-1000</label>
-          <label class="block text-gray-700">Nivel: <b>2</b></label>
+          <label class="block text-gray-700">Nivel: <b><%= @id %></b></label>
           <label class="block text-gray-700">Tipo: <b>Acumulativo</b></label>
           <div class="w-full inline-flex py-2">
 
             <div class="w-1/2 px-2">
-              <button class="py-2 bg-teal-800 text-teal-100 items-center inline-flex font-bold rounded text-sm w-full ">
+              <button phx-click="create_new" phx-value-id="<%= @id %>" phx-target="#sub_account-<%= @id %>" class="py-2 bg-teal-800 text-teal-100 items-center inline-flex font-bold rounded text-sm w-full ">
                 <svg aria-hidden="true" focusable="false" data-prefix="fas" data-icon="plus" role="img" xmlns="http://www.w3.org/2000/svg" viewBox="0 0 448 512"
                   class="h-4 w-4 mr-2 ml-auto">
                   <path fill="currentColor" d="M416 208H272V64c0-17.67-14.33-32-32-32h-32c-17.67 0-32 14.33-32 32v144H32c-17.67 0-32 14.33-32 32v32c0 17.67 14.33 32 32 32h144v144c0 17.67 14.33 32 32 32h32c17.67 0 32-14.33 32-32V304h144c17.67 0 32-14.33 32-32v-32c0-17.67-14.33-32-32-32z"
                   class="text-white">
                   </path>
                 </svg>
-                <label class="mr-auto text-white">Nueva</label>
+                <label class="cursor-pointer mr-auto text-white">Nueva</label>
               </button>
             </div>
 
