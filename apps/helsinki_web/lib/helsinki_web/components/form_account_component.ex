12da945c51ee7ff2c6ad957c707e439d42a66379
Héctor López <hector.lopez@DSA-DEV2-10>
2020-02-14 17:56:21 -0600

    cargar datos en formulario para editar

--- apps/helsinki_web/lib/helsinki_web/components/form_account_component.ex
+++ apps/helsinki_web/lib/helsinki_web/components/form_account_component.ex
@@ -8,12 +8,14 @@ defmodule AccountingSystemWeb.FormAccountComponent do
     levelx: 0,
     codex: "",
     parent_accountx: 0,
-    namex: ""
+    namex: "",
+    actionx: "",
+    parent_editx: %{}
     )}
   end
 
   def update(attrs, socket) do
-    attrs |> IO.inspect(label: "-------- >  Params Arrived ->")
+    IO.inspect(attrs.parent_edit, label: "VALORES EN ATTRS EDIT ---> ")
     values = if attrs.level == 0, do: load_values(), else: load_values_with_id(attrs.id)
     values |> IO.inspect(label: "VALUES -> -> ")
 
@@ -22,7 +24,9 @@ defmodule AccountingSystemWeb.FormAccountComponent do
       codex: values.code,
       parent_accountx: values.parent_account,
       root_accountx: values.root_account,
-      namex: (if attrs.level == 0, do: "", else: values.name)
+      namex: (if attrs.level == 0, do: "", else: values.name),
+      actionx: (if attrs.edit, do: "edit", else: ""),
+      parent_editx: (if attrs.edit, do: attrs.parent_edit, else: %{})
       )}
   end
 
@@ -43,7 +47,7 @@ defmodule AccountingSystemWeb.FormAccountComponent do
 
       <div class="inline-block bg-blue-700 text-white px-6 py-3 w-full">
         <h1 class="text-2xl font-medium text-white  block">
-          Nueva Cuenta
+          <%= (if @actionx == "edit", do: "Editar Cuenta", else: "Nueva Cuenta") %>
         </h1>
         <label class="block">Nivel: <b>2</b></label>
       </div>
@@ -54,21 +58,21 @@ defmodule AccountingSystemWeb.FormAccountComponent do
 
           <div class="px-8 py-6 flex flex-col my-2 w-160">
             <label class="block tracking-wide text-gray-700 font-bold" for="grid-code">Código</label>
-            <input type="text" name="code" maxlength="128" class="focus:outline-none focus:bg-white focus:border-blue-500 appearance-none block w-full bg-gray-200 text-gray-700 border rounded py-2 px-4 mb-3 leading-tight focus:outline-none focus:bg-white" id="grid-code" placeholder="Código" value="<%= @codex %>">
+            <input type="text" name="code" maxlength="128" class="focus:outline-none focus:bg-white focus:border-blue-500 appearance-none block w-full bg-gray-200 text-gray-700 border rounded py-2 px-4 mb-3 leading-tight focus:outline-none focus:bg-white" id="grid-code" placeholder="Código" value="<%= (if @actionx == "edit", do: @parent_editx.code, else: @codex) %>">
 
             <label class="block tracking-wide text-gray-700 font-bold" for="grid-name">Nombre</label>
             <div class="inline-flex w-full">
-              <input type="text" name="name" maxlength="32" class="focus:outline-none focus:bg-white focus:border-blue-500 appearance-none w-full bg-gray-200 text-gray-700 border rounded py-2 px-4 mb-3 leading-tight focus:outline-none focus:bg-white" id="grid-name" placeholder="Nombre" value="<%= @namex %>">
+              <input type="text" name="name" maxlength="32" class="focus:outline-none focus:bg-white focus:border-blue-500 appearance-none w-full bg-gray-200 text-gray-700 border rounded py-2 px-4 mb-3 leading-tight focus:outline-none focus:bg-white" id="grid-name" placeholder="Nombre" value="<%= (if @actionx == "edit", do: @parent_editx.name, else: @namex) %>">
               <input type="text" name="name2" maxlength="32" class="focus:outline-none focus:bg-white focus:border-blue-500 ml-4 appearance-none w-full bg-gray-200 text-gray-700 border rounded py-2 px-4 mb-3 leading-tight focus:outline-none focus:bg-white" id="grid-name" placeholder="Nombre">
             </div>
             <label class="block tracking-wide text-gray-700 font-bold" for="grid-description">Descripción</label>
-            <input type="text" name="description" maxlength="128" class="focus:outline-none focus:bg-white focus:border-blue-500 appearance-none block w-full bg-gray-200 text-gray-700 border rounded py-2 px-4 mb-3 leading-tight focus:outline-none focus:bg-white" id="grid-description" placeholder="Descripción">
+            <input type="text" name="description" maxlength="128" class="focus:outline-none focus:bg-white focus:border-blue-500 appearance-none block w-full bg-gray-200 text-gray-700 border rounded py-2 px-4 mb-3 leading-tight focus:outline-none focus:bg-white" id="grid-description" placeholder="Descripción" value="<%= (if @actionx == "edit", do: @parent_editx.description) %>">
 
             <label class="block tracking-wide text-gray-700 font-bold" for="grid-state">Tipo</label>
             <div class="relative mb-3">
               <select name="type" class="focus:outline-none focus:bg-white focus:border-blue-500 block appearance-none w-full bg-gray-200 border border-gray-200 text-gray-700 py-2 px-4 pr-8 rounded leading-tight" id="option-type">
-                <option value="A">Acumulativo</option>
-                <option value="D">Detalle</option>
+                <option value="A" <%= (if @actionx == "edit", do: (if @parent_editx.type == "A", do: "selected", else: "unselected")) %> >Acumulativo</option>
+                <option value="D" <%= (if @actionx == "edit", do: (if @parent_editx.type == "D", do: "selected", else: "unselected")) %> >Detalle</option>
               </select>
               <div class="pointer-events-none absolute inset-y-0 right-0 flex items-center px-2 text-gray-700">
                 <svg class="fill-current h-4 w-4" xmlns="http://www.w3.org/2000/svg" viewBox="0 0 20 20"><path d="M9.293 12.95l.707.707L15.657 8l-1.414-1.414L10 10.828 5.757 6.586 4.343 8z"/></svg>
@@ -77,27 +81,27 @@ defmodule AccountingSystemWeb.FormAccountComponent do
             <label class="block tracking-wide text-gray-700 font-bold" for="uuid-voucher">
               Vale Uuid
             </label>
-            <input type="text" name="uuid_voucher" maxlength="32" class="focus:outline-none focus:bg-white focus:border-blue-500 appearance-none block w-full bg-gray-200 text-gray-700 border rounded py-2 px-4 mb-3 leading-tight focus:outline-none focus:bg-white" id="grid-uuid-voucher" placeholder="Vale Uuid">
+            <input type="text" name="uuid_voucher" maxlength="32" class="focus:outline-none focus:bg-white focus:border-blue-500 appearance-none block w-full bg-gray-200 text-gray-700 border rounded py-2 px-4 mb-3 leading-tight focus:outline-none focus:bg-white" id="grid-uuid-voucher" placeholder="Vale Uuid" value="<%= (if @actionx == "edit", do: @parent_editx.uuid_voucher) %>">
 
             <div class="inline-flex w-full">
               <div class="inline-block w-full mr-2">
                 <label class="block tracking-wide text-gray-700 font-bold" for="grid-name">Código de Grupo</label>
-                <input type="number" name="group_code" min="1" class="focus:outline-none focus:bg-white focus:border-blue-500 appearance-none w-full bg-gray-200 text-gray-700 border rounded py-2 px-4 mb-3 leading-tight focus:outline-none focus:bg-white" id="grid-name" placeholder="Código de Grupo">
+                <input type="number" name="group_code" min="1" class="focus:outline-none focus:bg-white focus:border-blue-500 appearance-none w-full bg-gray-200 text-gray-700 border rounded py-2 px-4 mb-3 leading-tight focus:outline-none focus:bg-white" id="grid-name" placeholder="Código de Grupo" value="<%= (if @actionx == "edit", do: @parent_editx.group_code) %>">
               </div>
               <div class="inline-block w-full ml-2">
                 <label class="block tracking-wide text-gray-700 font-bold" for="grid-name">Aplicar a</label>
-                <input type="number" name="apply_to" min="1" class="focus:outline-none focus:bg-white focus:border-blue-500 appearance-none w-full bg-gray-200 text-gray-700 border rounded py-2 px-4 mb-3 leading-tight focus:outline-none focus:bg-white" id="grid-name" placeholder="Aplicar a">
+                <input type="number" name="apply_to" min="1" class="focus:outline-none focus:bg-white focus:border-blue-500 appearance-none w-full bg-gray-200 text-gray-700 border rounded py-2 px-4 mb-3 leading-tight focus:outline-none focus:bg-white" id="grid-name" placeholder="Aplicar a" value="<%= (if @actionx == "edit", do: @parent_editx.apply_to) %>">
               </div>
             </div>
 
             <div class="inline-flex w-full">
               <div class="inline-block w-full mr-2">
-                <label class="block tracking-wide text-gray-700 font-bold" for="grid-name">Fiscar Tercero</label>
-                <input type="number" name="third_party_prosecutor" min="1" class="focus:outline-none focus:bg-white focus:border-blue-500 appearance-none w-full bg-gray-200 text-gray-700 border rounded py-2 px-4 mb-3 leading-tight focus:outline-none focus:bg-white" id="grid-name" placeholder="Fiscal Tercero">
+                <label class="block tracking-wide text-gray-700 font-bold" for="grid-name">Fiscal Tercero</label>
+                <input type="number" name="third_party_prosecutor" min="1" class="focus:outline-none focus:bg-white focus:border-blue-500 appearance-none w-full bg-gray-200 text-gray-700 border rounded py-2 px-4 mb-3 leading-tight focus:outline-none focus:bg-white" id="grid-name" placeholder="Fiscal Tercero"  value="<%= (if @actionx == "edit", do: @parent_editx.third_party_prosecutor) %>">
               </div>
               <div class="inline-block w-full ml-2">
-                <label class="block tracking-wide text-gray-700 font-bold" for="grid-name">Aplicar Tercero a</label>
-                <input type="text" name="apply_third_party_to" maxlength="2" class="focus:outline-none focus:bg-white focus:border-blue-500 appearance-none w-full bg-gray-200 text-gray-700 border rounded py-2 px-4 mb-3 leading-tight focus:outline-none focus:bg-white" id="grid-name" placeholder="Aplicar Tercero a">
+                <label class="block tracking-wide text-gray-700 font-bold" for="grid-name">Aplicar a Tercero</label>
+                <input type="text" name="apply_third_party_to" maxlength="2" class="focus:outline-none focus:bg-white focus:border-blue-500 appearance-none w-full bg-gray-200 text-gray-700 border rounded py-2 px-4 mb-3 leading-tight focus:outline-none focus:bg-white" id="grid-name" placeholder="Aplicar Tercero a"  value="<%= (if @actionx == "edit", do: @parent_editx.apply_third_party_to) %>">
               </div>
             </div>
 
@@ -106,35 +110,35 @@ defmodule AccountingSystemWeb.FormAccountComponent do
           <div class="px-8 py-6 flex flex-col my-2 w-80">
             <div class="inline-flex items-center py-6">
               <div class="modern-checkbox flex shadow-md h-6 w-12 rounded-full " >
-                <input type="checkbox" name="status" class="hidden" id="checkbox-active" checked value="A">
+                <input type="checkbox" name="status" class="hidden" id="checkbox-active" <%= (if @actionx == "edit", do: (if @parent_editx.status == "A", do: "checked", else: "unchecked"), else: "checked") %> >
                 <label class="relative inline bg-transparent w-10" for="checkbox-active"></label>
               </div>
               <label class="ml-2 font-bold text-gray-700">Activo</label>
             </div>
             <div class="inline-flex items-center py-6">
               <div class="modern-checkbox flex shadow-md h-6 w-12 rounded-full " >
-                <input type="checkbox" name="is_departamental" class="hidden" id="checkbox-departamental">
+                <input type="checkbox" name="is_departamental" class="hidden" id="checkbox-departamental" <%= (if @actionx == "edit", do: (if @parent_editx.is_departamental, do: "checked", else: "unchecked")) %> >
                 <label class="relative inline bg-transparent w-10" for="checkbox-departamental"></label>
               </div>
               <label class="ml-2 font-bold text-gray-700">Es Departamental</label>
             </div>
             <div class="inline-flex items-center py-6">
               <div class="modern-checkbox flex shadow-md h-6 w-12 rounded-full " >
-                <input type="checkbox" name="character" class="hidden" id="checkbox-character">
+                <input type="checkbox" name="character" class="hidden" id="checkbox-character" <%= (if @actionx == "edit", do: (if @parent_editx.character, do: "checked", else: "unchecked")) %> >
                 <label class="relative inline bg-transparent w-10" for="checkbox-character"></label>
               </div>
               <label class="ml-2 font-bold text-gray-700">Caracter</label>
             </div>
             <div class="inline-flex items-center py-6">
               <div class="modern-checkbox flex shadow-md h-6 w-12 rounded-full " >
-                <input type="checkbox" name="third_party_op" class="hidden" id="checkbox-third-party-op">
+                <input type="checkbox" name="third_party_op" class="hidden" id="checkbox-third-party-op" <%= (if @actionx == "edit", do: (if @parent_editx.third_party_op, do: "checked", else: "unchecked")) %> >
                 <label class="relative inline bg-transparent w-10" for="checkbox-third-party-op"></label>
               </div>
               <label class="ml-2 font-bold text-gray-700">Operación de Terceros</label>
             </div>
             <div class="inline-flex items-center py-6">
               <div class="modern-checkbox flex shadow-md h-6 w-12 rounded-full " >
-                <input type="checkbox" name="payment_method" class="hidden" id="checkbox-payment-method">
+                <input type="checkbox" name="payment_method" class="hidden" id="checkbox-payment-method" <%= (if @actionx == "edit", do: (if @parent_editx.payment_method, do: "checked", else: "unchecked")) %> >
                 <label class="relative inline bg-transparent w-10" for="checkbox-payment-method"></label>
               </div>
               <label class="ml-2 font-bold text-gray-700">Método de Pago</label>
--- apps/helsinki_web/lib/helsinki_web/components/sub_accounts_component.ex
+++ apps/helsinki_web/lib/helsinki_web/components/sub_accounts_component.ex
@@ -34,7 +34,7 @@ defmodule AccountingSystemWeb.SubAccountsComponent do
           <div class="w-full inline-flex py-2">
 
             <div class="w-1/2 px-2">
-              <button  phx-click="create_new" phx-value-level="<%= @level %>" phx-target="#one" class="py-2 bg-teal-500 text-white hover:bg-teal-400 items-center inline-flex font-bold rounded text-sm w-full ">
+              <button phx-click="create_new" phx-value-level="<%= @level %>" phx-target="#one" class="py-2 bg-teal-500 text-white hover:bg-teal-400 items-center inline-flex font-bold rounded text-sm w-full ">
                 <svg aria-hidden="true" focusable="false" data-prefix="fas" data-icon="plus" role="img" xmlns="http://www.w3.org/2000/svg" viewBox="0 0 448 512"
                   class="h-4 w-4 mr-2 ml-auto">
                   <path fill="currentColor" d="M416 208H272V64c0-17.67-14.33-32-32-32h-32c-17.67 0-32 14.33-32 32v144H32c-17.67 0-32 14.33-32 32v32c0 17.67 14.33 32 32 32h144v144c0 17.67 14.33 32 32 32h32c17.67 0 32-14.33 32-32V304h144c17.67 0 32-14.33 32-32v-32c0-17.67-14.33-32-32-32z"
@@ -46,7 +46,7 @@ defmodule AccountingSystemWeb.SubAccountsComponent do
             </div>
 
             <div class="w-1/2 px-2">
-              <button class="py-2 bg-teal-500 text-white hover:bg-teal-400 items-center inline-flex font-bold rounded text-sm w-full ">
+              <button phx-click="edit_this" phx-value-level="<%= @level %>" phx-value-code="<%= @code %>" phx-target="#one" class="py-2 bg-teal-500 text-white hover:bg-teal-400 items-center inline-flex font-bold rounded text-sm w-full ">
                 <svg aria-hidden="true" focusable="false" data-prefix="fas" data-icon="pencil-alt" role="img" xmlns="http://www.w3.org/2000/svg" viewBox="0 0 512 512"
                   class="h-4 w-4 mr-2 ml-auto">
                   <path fill="currentColor" d="M497.9 142.1l-46.1 46.1c-4.7 4.7-12.3 4.7-17 0l-111-111c-4.7-4.7-4.7-12.3 0-17l46.1-46.1c18.7-18.7 49.1-18.7 67.9 0l60.1 60.1c18.8 18.7 18.8 49.1 0 67.9zM284.2 99.8L21.6 362.4.4 483.9c-2.9 16.4 11.4 30.6 27.8 27.8l121.5-21.3 262.6-262.6c4.7-4.7 4.7-12.3 0-17l-111-111c-4.8-4.7-12.4-4.7-17.1 0zM124.1 339.9c-5.5-5.5-5.5-14.3 0-19.8l154-154c5.5-5.5 14.3-5.5 19.8 0s5.5 14.3 0 19.8l-154 154c-5.5 5.5-14.3 5.5-19.8 0zM88 424h48v36.3l-64.5 11.3-31.1-31.1L51.7 376H88v48z"
