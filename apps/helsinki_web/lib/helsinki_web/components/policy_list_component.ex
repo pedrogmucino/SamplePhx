1f6791ab81826e00e93846c04c2d87147690cc04
erik.gonzalez <erik.gonzalez@santiago.mx>
2020-03-03 16:36:09 -0600

    correccion de render

--- apps/helsinki_web/lib/helsinki_web/components/policy_list_component.ex
+++ apps/helsinki_web/lib/helsinki_web/components/policy_list_component.ex
@@ -7,69 +7,6 @@ defmodule AccountingSystemWeb.PolicyListComponent do
   }
 
 
-  def render(assigns) do
-    ~L"""
-    <%= if @message do %>
-      <%= live_component(@socket, AccountingSystemWeb.NotificationComponent, id: "notification", message: @message, show: true) %>
-    <% end %>
-    <%= if @cancel? do %>
-      <%= live_component(@socket, AccountingSystemWeb.ConfirmationComponent, id: "notification", message: @message, show: true) %>
-    <% end %>
-    <div id="one" class="bg-white h-hoch-93 w-80 mt-16 ml-16 block float-left">
-      <div class="w-full py-2 bg-blue-700">
-        <p class="ml-2 font-bold text-lg text-white">Pólizas</p>
-      </div>
-    <div class="relative w-full px-2 mt-4">
-      <input class="focus:outline-none focus:bg-white focus:border-blue-500 h-8 w-full rounded border bg-gray-300 pl-2" placeholder="Buscar Póliza" >
-      <svg aria-hidden="true" focusable="false" data-prefix="fad" data-icon="search" role="img" xmlns="http://www.w3.org/2000/svg" viewBox="0 0 512 512"
-        class="absolute right-0 top-0 h-4 w-4 mr-4 mt-2">
-        <g>
-          <path fill="currentColor" d="M208 80a128 128 0 1 1-90.51 37.49A127.15 127.15 0 0 1 208 80m0-80C93.12 0 0 93.12 0 208s93.12 208 208 208 208-93.12 208-208S322.88 0 208 0z"
-          class="text-gray-600">
-          </path>
-          <path fill="currentColor" d="M504.9 476.7L476.6 505a23.9 23.9 0 0 1-33.9 0L343 405.3a24 24 0 0 1-7-17V372l36-36h16.3a24 24 0 0 1 17 7l99.7 99.7a24.11 24.11 0 0 1-.1 34z"
-          class="text-gray-500">
-          </path>
-        </g>
-      </svg>
-    </div>
-
-    <div class="w-1/2 px-2 mt-2">
-      <button phx-click="create_new" phx-value-id="xxx" phx-target="#one" class="py-2 bg-teal-500 hover:bg-teal-400 text-white items-center inline-flex font-bold rounded text-sm w-full ">
-        <svg aria-hidden="true" focusable="false" data-prefix="fas" data-icon="plus" role="img" xmlns="http://www.w3.org/2000/svg" viewBox="0 0 448 512"
-        class="h-4 w-4 mr-2 ml-auto">
-          <path fill="currentColor" d="M416 208H272V64c0-17.67-14.33-32-32-32h-32c-17.67 0-32 14.33-32 32v144H32c-17.67 0-32 14.33-32 32v32c0 17.67 14.33 32 32 32h144v144c0 17.67 14.33 32 32 32h32c17.67 0 32-14.33 32-32V304h144c17.67 0 32-14.33 32-32v-32c0-17.67-14.33-32-32-32z"
-          class="text-white">
-          </path>
-        </svg>
-        <label class="cursor-pointer mr-auto text-white">Nueva</label>
-      </button>
-    </div>
-
-    <div class="h-hoch-75 overflow-y-scroll pb-16">
-      <%= for item <- @policy_list do %>
-        <div class="w-full px-2 block">
-          <div phx-click="open_policy" phx-value-id="<%= item.id %>" phx-target="#one" class="border cursor-pointer w-full block bg-gray-200 p-3 mt-2 rounded relative hover:bg-gray-300">
-            <h2 class="text-gray-700 text-xl"><%= item.serial  %>-<%= item.policy_number %></h2>
-            <label class="inline-block cursor-pointer text-gray-600 font-bold text-sm">Concepto: <b><%= item.concept %></b></label></br>
-            <label class="inline-block cursor-pointer text-gray-600 font-bold text-sm">Tipo: <b><%= item.type_description %></b></label>
-          </div>
-        </div>
-
-      <% end %>
-    </div>
-    </div>
-
-    <%= if @new? do %>
-      <%= live_component(@socket, AccountingSystemWeb.NewPolicyComponent, id: 0, update_text: @update_text, pollys: @pollys, arr: @arr, edit: false, update: @update) %>
-    <% end %>
-
-    <%= if @edit? do %>
-      <%= live_component(@socket, AccountingSystemWeb.NewPolicyComponent, id: @policy_id, update_text: "", pollys: @pollys, arr: @arr, edit: true, update: @update) %>
-    <% end %>
-    """
-  end
-
   def mount(socket) do
     {:ok, assign(socket,
     policy_list: PolicyHandler.get_policy_list,
@@ -354,6 +291,15 @@ defmodule AccountingSystemWeb.PolicyListComponent do
   defp get_max_id([]) do
     0
   end
+
+  defp get_max_id(arr) do
+    Enum.max_by(arr, fn x -> x.id end).id + 1
+  end
+
+  defp to_float(x) when is_bitstring(x), do: void(x)
+  defp to_float(x) when is_integer(x), do: x/1
+  defp to_float(x) when is_float(x), do: x
+
   def render(assigns) do
     ~L"""
     <%= if @message do %>
@@ -401,19 +347,16 @@ defmodule AccountingSystemWeb.PolicyListComponent do
           </div>
         </div>
 
-  defp get_max_id(arr) do
-    Enum.max_by(arr, fn x -> x.id end).id + 1
-  end
+      <% end %>
+    </div>
+    </div>
 
-  defp to_float(x) when is_bitstring(x), do: void(x)
-  defp to_float(x) when is_integer(x), do: x/1
-  defp to_float(x) when is_float(x), do: x
     <%= if @new? do %>
-      <%= live_component(@socket, AccountingSystemWeb.NewPolicyComponent, id: 0, update_text: @update_text, pollys: @pollys, arr: @arr, edit: false, update: @update, cancel?: false, message_confirm: nil) %>
+      <%= live_component(@socket, AccountingSystemWeb.NewPolicyComponent, id: 0, update_text: @update_text, pollys: @pollys, arr: @arr, edit: false, update: @update) %>
     <% end %>
 
     <%= if @edit? do %>
-      <%= live_component(@socket, AccountingSystemWeb.NewPolicyComponent, id: @policy_id, update_text: "", pollys: @pollys, arr: [], edit: true, update: @update, cancel?: @cancel?, message_confirm: @message_confirm) %>
+      <%= live_component(@socket, AccountingSystemWeb.NewPolicyComponent, id: @policy_id, update_text: "", pollys: @pollys, arr: @arr, edit: true, update: @update) %>
     <% end %>
     """
   end
