b631f0417f14f54006329e2040bd2779994546a0
Héctor López <hector.lopez@DSA-DEV2-10>
2020-03-05 16:30:10 -0600

    disabled form policy cancel

--- apps/helsinki_web/lib/helsinki_web/components/new_policy_component.ex
+++ apps/helsinki_web/lib/helsinki_web/components/new_policy_component.ex
@@ -109,12 +109,23 @@ defmodule AccountingSystemWeb.NewPolicyComponent do
                       <% end %>
                     </div>
                     <input <%= if !@status, do: 'disabled' %> type="text" name="name" maxlength="128" value="<%=@pollys.name%>" class="focus:outline-none focus:bg-white focus:border-blue-500 ml-4 appearance-none w-1/3 bg-gray-200 text-gray-700 border rounded py-2 px-4 mb-3 leading-tight focus:outline-none focus:bg-white" id="grid-name">
-                    <a href="/account" target="_blank" class="tooltip w-10 h-hoch-2 px-4 ml-2 bg-teal-500 text-white text-center hover:bg-teal-400 border rounded">
-                      <svg aria-hidden="true" focusable="false" data-prefix="far" data-icon="layer-plus" role="img" xmlns="http://www.w3.org/2000/svg" viewBox="0 0 512 512"
-                        class="h-6 w-6 mr-auto -ml-2 mt-1"><path fill="currentColor" d="M492.88 354.58L413.19 320l79.68-34.58c12.16-5.28 17.72-19.41 12.47-31.56-5.28-12.17-19.38-17.67-31.59-12.47l-217.22 94.72L71.91 256l170.5-73.98c12.16-5.28 17.72-19.41 12.47-31.56-5.28-12.19-19.38-17.67-31.59-12.47L19.16 226.56C7.53 231.59 0 243.16 0 256s7.53 24.41 19.12 29.42L98.82 320l-79.67 34.56C7.53 359.59 0 371.16 0 384.02c0 12.84 7.53 24.41 19.12 29.42l218.28 94.69a46.488 46.488 0 0 0 18.59 3.88c6.34-.02 12.69-1.3 18.59-3.86l218.25-94.69c11.62-5.03 19.16-16.59 19.16-29.44.01-12.86-7.52-24.43-19.11-29.44zM256.53 464.11L71.91 384l87.22-37.84 78.28 33.96c5.91 2.58 12.25 3.86 18.59 3.86s12.69-1.28 18.59-3.84l78.3-33.98 87.29 37.88-183.65 80.07zM496 88h-72V16c0-8.84-7.16-16-16-16h-16c-8.84 0-16 7.16-16 16v72h-72c-8.84 0-16 7.16-16 16v16c0 8.84 7.16 16 16 16h72v72c0 8.84 7.16 16 16 16h16c8.84 0 16-7.16 16-16v-72h72c8.84 0 16-7.16 16-16v-16c0-8.84-7.16-16-16-16z" class=""></path>
-                      </svg>
-                      <span class='tooltip-text text-white bg-blue-500 mt-5 -ml-12 rounded'>Nueva Cuenta</span>
-                    </a>
+
+                    <div class=" <%= if !@status, do: 'opacity-50 cursor-not-allowed' %> tooltip w-10 h-hoch-2 px-4 ml-2 bg-teal-500 text-white text-center hover:bg-teal-400 border rounded">
+                      <%= if !@status do %>
+                        <svg aria-hidden="true" focusable="false" data-prefix="far" data-icon="layer-plus" role="img" xmlns="http://www.w3.org/2000/svg" viewBox="0 0 512 512"
+                            class="h-6 w-6 mr-auto -ml-2 mt-1"><path fill="currentColor" d="M492.88 354.58L413.19 320l79.68-34.58c12.16-5.28 17.72-19.41 12.47-31.56-5.28-12.17-19.38-17.67-31.59-12.47l-217.22 94.72L71.91 256l170.5-73.98c12.16-5.28 17.72-19.41 12.47-31.56-5.28-12.19-19.38-17.67-31.59-12.47L19.16 226.56C7.53 231.59 0 243.16 0 256s7.53 24.41 19.12 29.42L98.82 320l-79.67 34.56C7.53 359.59 0 371.16 0 384.02c0 12.84 7.53 24.41 19.12 29.42l218.28 94.69a46.488 46.488 0 0 0 18.59 3.88c6.34-.02 12.69-1.3 18.59-3.86l218.25-94.69c11.62-5.03 19.16-16.59 19.16-29.44.01-12.86-7.52-24.43-19.11-29.44zM256.53 464.11L71.91 384l87.22-37.84 78.28 33.96c5.91 2.58 12.25 3.86 18.59 3.86s12.69-1.28 18.59-3.84l78.3-33.98 87.29 37.88-183.65 80.07zM496 88h-72V16c0-8.84-7.16-16-16-16h-16c-8.84 0-16 7.16-16 16v72h-72c-8.84 0-16 7.16-16 16v16c0 8.84 7.16 16 16 16h72v72c0 8.84 7.16 16 16 16h16c8.84 0 16-7.16 16-16v-72h72c8.84 0 16-7.16 16-16v-16c0-8.84-7.16-16-16-16z" class=""></path>
+                        </svg>
+                        <span class='tooltip-text text-white bg-blue-500 mt-5 -ml-12 rounded'>Nueva Cuenta</span>
+                      <% else %>
+                        <a href="/account" target="_blank">
+                          <svg aria-hidden="true" focusable="false" data-prefix="far" data-icon="layer-plus" role="img" xmlns="http://www.w3.org/2000/svg" viewBox="0 0 512 512"
+                            class="h-6 w-6 mr-auto -ml-2 mt-1"><path fill="currentColor" d="M492.88 354.58L413.19 320l79.68-34.58c12.16-5.28 17.72-19.41 12.47-31.56-5.28-12.17-19.38-17.67-31.59-12.47l-217.22 94.72L71.91 256l170.5-73.98c12.16-5.28 17.72-19.41 12.47-31.56-5.28-12.19-19.38-17.67-31.59-12.47L19.16 226.56C7.53 231.59 0 243.16 0 256s7.53 24.41 19.12 29.42L98.82 320l-79.67 34.56C7.53 359.59 0 371.16 0 384.02c0 12.84 7.53 24.41 19.12 29.42l218.28 94.69a46.488 46.488 0 0 0 18.59 3.88c6.34-.02 12.69-1.3 18.59-3.86l218.25-94.69c11.62-5.03 19.16-16.59 19.16-29.44.01-12.86-7.52-24.43-19.11-29.44zM256.53 464.11L71.91 384l87.22-37.84 78.28 33.96c5.91 2.58 12.25 3.86 18.59 3.86s12.69-1.28 18.59-3.84l78.3-33.98 87.29 37.88-183.65 80.07zM496 88h-72V16c0-8.84-7.16-16-16-16h-16c-8.84 0-16 7.16-16 16v72h-72c-8.84 0-16 7.16-16 16v16c0 8.84 7.16 16 16 16h72v72c0 8.84 7.16 16 16 16h16c8.84 0 16-7.16 16-16v-72h72c8.84 0 16-7.16 16-16v-16c0-8.84-7.16-16-16-16z" class=""></path>
+                          </svg>
+                          <span class='tooltip-text text-white bg-blue-500 mt-5 -ml-12 rounded'>Nueva Cuenta</span>
+                        </a>
+                      <% end %>
+                    </div>
+
                   </div>
 
                   <label class="block tracking-wide text-gray-700 font-bold" for="grid-code">Concepto</label>
@@ -135,7 +146,7 @@ defmodule AccountingSystemWeb.NewPolicyComponent do
                         <input <%= if !@status, do: 'disabled' %> type="number" name="credit" step="0.01" min="0" max="999999.99" phx-hook="format_number" value="<%=@pollys.credit%>" class="focus:outline-none focus:bg-white focus:border-blue-500 appearance-none w-full bg-gray-200 text-gray-700 border rounded py-2 px-4 mb-3 leading-tight focus:outline-none focus:bg-white" id="grid-name">
                       </div>
                       <div class="inline-block mt-6 ml-2 text-right w-1/6">
-                        <button <%= if !@status, do: 'disabled' %> name="save_aux" class="bg-teal-500 text-white text-center hover:bg-teal-400 phx-target="#one" border rounded">
+                        <button <%= if !@status, do: 'disabled' %> name="save_aux" class=" <%= if !@status, do: 'opacity-50 cursor-not-allowed' %> bg-teal-500 text-white text-center hover:bg-teal-400 phx-target="#one" border rounded">
                         <svg aria-hidden="true" focusable="false" data-prefix="fas" data-icon="share-square" role="img" xmlns="http://www.w3.org/2000/svg" viewBox="0 0 576 512" class="h-6 w-6 m-1"><path fill="currentColor" d="M568.482 177.448L424.479 313.433C409.3 327.768 384 317.14 384 295.985v-71.963c-144.575.97-205.566 35.113-164.775 171.353 4.483 14.973-12.846 26.567-25.006 17.33C155.252 383.105 120 326.488 120 269.339c0-143.937 117.599-172.5 264-173.312V24.012c0-21.174 25.317-31.768 40.479-17.448l144.003 135.988c10.02 9.463 10.028 25.425 0 34.896zM384 379.128V448H64V128h50.916a11.99 11.99 0 0 0 8.648-3.693c14.953-15.568 32.237-27.89 51.014-37.676C185.708 80.83 181.584 64 169.033 64H48C21.49 64 0 85.49 0 112v352c0 26.51 21.49 48 48 48h352c26.51 0 48-21.49 48-48v-88.806c0-8.288-8.197-14.066-16.011-11.302a71.83 71.83 0 0 1-34.189 3.377c-7.27-1.046-13.8 4.514-13.8 11.859z" class=""></path></svg>
                         </button>
                       </div>
@@ -148,7 +159,7 @@ defmodule AccountingSystemWeb.NewPolicyComponent do
                 <!---------------------------------GUARDAR - ELIMINAR -------------------------------------->
                 <div class="pt-24 grid grid-cols-2 flex">
                   <div class="flex-1 text-center">
-                  <%=if @pollys.total != 0 or length(@arr) == 0 do %>
+                  <%=if (@pollys.total != 0 or length(@arr) == 0) or (!@status) do %>
                     <button form="form1" disabled class="opacity-50 cursor-not-allowed py-2 w-1/2 bg-teal-500 text-white items-center inline-flex font-bold rounded shadow focus:shadow-outline focus:outline-none rounded">
                   <% else %>
                     <button form="form1" class="py-2 w-1/2 bg-teal-500 text-white hover:bg-teal-400 items-center inline-flex font-bold rounded shadow focus:shadow-outline focus:outline-none rounded">
@@ -164,13 +175,13 @@ defmodule AccountingSystemWeb.NewPolicyComponent do
                           </path>
                         </g>
                       </svg>
-                        <label class="<%=if @pollys.total != 0 or length(@arr) == 0, do: "cursor-not-allowed", else: "cursor-pointer" %> mr-auto text-white">Guardar</label>
+                        <label class="<%=if (@pollys.total != 0 or length(@arr) == 0) or (!@status), do: "cursor-not-allowed", else: "cursor-pointer" %> mr-auto text-white">Guardar</label>
                     </button>
                   </div>
                   <div class="flex-1 text-center">
                   <%= if @edit do %>
-                      <button phx-click="delete_policy" phx-target="#policy" phx-value-id="<%=@pollys.id%>" phx-value-delete="true"
-                        class="py-2 w-1/2 bg-red-500 text-white hover:bg-red-400 items-center inline-flex font-bold rounded shadow focus:shadow-outline focus:outline-none rounded">
+                      <button <%= if !@status, do: 'disabled' %> phx-click="delete_policy" phx-target="#policy" phx-value-id="<%=@pollys.id%>" phx-value-delete="true"
+                        class=" <%= if !@status, do: 'opacity-50 cursor-not-allowed' %> py-2 w-1/2 bg-red-500 text-white hover:bg-red-400 items-center inline-flex font-bold rounded shadow focus:shadow-outline focus:outline-none rounded">
                         <svg aria-hidden="true" focusable="false" data-prefix="fad" data-icon="trash-alt" role="img" xmlns="http://www.w3.org/2000/svg" viewBox="0 0 448 512"
                           class="h-4 w-4 mr-2 ml-auto">
                           <g class="fa-group">
@@ -182,7 +193,7 @@ defmodule AccountingSystemWeb.NewPolicyComponent do
                             </path>
                           </g>
                         </svg>
-                        <label class="cursor-pointer mr-auto text-white">Cancelar</label>
+                        <label class=" <%= if !@status, do: 'cursor-not-allowed', else: 'cursor-pointer' %> mr-auto text-white">Cancelar</label>
                       </button>
                     <% end %>
                   </div>
@@ -238,10 +249,10 @@ defmodule AccountingSystemWeb.NewPolicyComponent do
                           </div>
                           <!-----------------------------------END INFO--------------------------------->
                           <div class="w-1/6 text-right">
-                            <button phx-click="edit_aux" value="<%=item.id%>" phx-target="#one" class="bg-teal-500 text-white text-center hover:bg-teal-400 border rounded">
+                            <button <%= if !@status, do: 'disabled' %> phx-click="edit_aux" value="<%=item.id%>" phx-target="#one" class="<%= if !@status, do: 'cursor-not-allowed' %> bg-teal-500 text-white text-center hover:bg-teal-400 border rounded">
                               <svg aria-hidden="true" focusable="false" data-prefix="fal" data-icon="edit" role="img" xmlns="http://www.w3.org/2000/svg" viewBox="0 0 576 512" class="h-6 w-6 m-1"><path fill="currentColor" d="M402.3 344.9l32-32c5-5 13.7-1.5 13.7 5.7V464c0 26.5-21.5 48-48 48H48c-26.5 0-48-21.5-48-48V112c0-26.5 21.5-48 48-48h273.5c7.1 0 10.7 8.6 5.7 13.7l-32 32c-1.5 1.5-3.5 2.3-5.7 2.3H48v352h352V350.5c0-2.1.8-4.1 2.3-5.6zm156.6-201.8L296.3 405.7l-90.4 10c-26.2 2.9-48.5-19.2-45.6-45.6l10-90.4L432.9 17.1c22.9-22.9 59.9-22.9 82.7 0l43.2 43.2c22.9 22.9 22.9 60 .1 82.8zM460.1 174L402 115.9 216.2 301.8l-7.3 65.3 65.3-7.3L460.1 174zm64.8-79.7l-43.2-43.2c-4.1-4.1-10.8-4.1-14.8 0L436 82l58.1 58.1 30.9-30.9c4-4.2 4-10.8-.1-14.9z" class=""></path></svg>
                             </button>
-                            <button phx-click="delete_aux" value="<%=item.id%>" phx-target="#one" class="bg-red-500 text-white text-left hover:bg-red-400 border rounded">
+                            <button <%= if !@status, do: 'disabled' %> phx-click="delete_aux" value="<%=item.id%>" phx-target="#one" class="<%= if !@status, do: 'cursor-not-allowed' %> bg-red-500 text-white text-left hover:bg-red-400 border rounded">
                               <svg aria-hidden="true" focusable="false" data-prefix="fas" data-icon="minus" role="img" xmlns="http://www.w3.org/2000/svg" viewBox="0 0 448 512" class="h-6 w-6 m-1"><path fill="currentColor" d="M416 208H32c-17.67 0-32 14.33-32 32v32c0 17.67 14.33 32 32 32h384c17.67 0 32-14.33 32-32v-32c0-17.67-14.33-32-32-32z" class=""></path></svg>
                             </button>
                           </div>
