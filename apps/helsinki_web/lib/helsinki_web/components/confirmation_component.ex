ae57ae821982e4ca383b5826f219039b59696886
Héctor López <hector.lopez@DSA-DEV2-10>
2020-03-06 10:49:21 -0600

    animacion de msj confirmacion

--- apps/helsinki_web/lib/helsinki_web/components/confirmation_component.ex
+++ apps/helsinki_web/lib/helsinki_web/components/confirmation_component.ex
@@ -10,9 +10,9 @@ defmodule AccountingSystemWeb.ConfirmationComponent do
   def render(assigns) do
     ~L"""
     <%= if @show do %>
-    <div id="x" class="z-10 fixed flex inset-0">
+    <div id="x" class="z-10 fixed flex inset-0 zum">
       <div class="mt-64 mb-64 py-64 ml-auto mr-auto">
-        <div class="w-160 bg-yellow-100 border-t-4 border-yellow-500 rounded-b text-yellow-900 px-4 py-3 shadow-md" role="alert">
+        <div class="w-160 bg-yellow-100 mt-10 border-t-4 border-yellow-500 rounded-b text-yellow-900 px-4 py-3 shadow-md">
           <div class="flex">
             <div class="py-1"><svg class="fill-current h-6 w-6 text-yellow-500 mr-4" xmlns="http://www.w3.org/2000/svg" viewBox="0 0 20 20"><path d="M2.93 17.07A10 10 0 1 1 17.07 2.93 10 10 0 0 1 2.93 17.07zm12.73-1.41A8 8 0 1 0 4.34 4.34a8 8 0 0 0 11.32 11.32zM9 11V9h2v6H9v-4zm0-6h2v2H9V5z"/></svg></div>
             <div>
