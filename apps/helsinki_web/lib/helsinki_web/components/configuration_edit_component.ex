7753b4829130c368b8de568db2999a7166b42ab6
pedrogmucino <enriquegmucino@gmail.com>
2020-03-12 11:21:32 -0600

    Corrijo posición botón eliminar configuración

--- apps/helsinki_web/lib/helsinki_web/components/configuration_edit_component.ex
+++ apps/helsinki_web/lib/helsinki_web/components/configuration_edit_component.ex
@@ -63,9 +63,9 @@ defmodule AccountingSystemWeb.ConfigurationEditComponent do
           </div>
         </form>
 
-        <div class="inline-flex w-full py-16 absolute bottom-0 right-0 pr-0">
+        <div class="inline-flex w-1/2 h-10 mb-16 absolute bottom-0 right-0 pr-0">
         <button phx-click="delete_structure" phx-target="#comp" phx-value-id=<%= @structure.id %>
-            class= "ml-mar-44 py-2 w-32 bg-red-500 text-white hover:bg-red-400 items-center inline-flex font-bold rounded shadow focus:shadow-outline focus:outline-none rounded">
+            class= "ml-2 py-2 w-32 bg-red-500 text-white hover:bg-red-400 items-center inline-flex font-bold rounded shadow focus:shadow-outline focus:outline-none rounded">
               <svg aria-hidden="true" focusable="false" data-prefix="fad" data-icon="trash-alt" role="img" xmlns="http://www.w3.org/2000/svg" viewBox="0 0 448 512"
                 class="h-4 w-4 mr-2 ml-auto">
                 <g class="fa-group">
