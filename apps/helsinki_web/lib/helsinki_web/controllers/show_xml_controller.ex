fa76eb73d81409a7432c3d30696d8f85a6c613b1
Héctor López <hector.lopez@DSA-DEV2-10>
2020-03-26 12:11:06 -0600

    clean code xml download

--- apps/helsinki_web/lib/helsinki_web/controllers/show_xml_controller.ex
+++ apps/helsinki_web/lib/helsinki_web/controllers/show_xml_controller.ex
@@ -2,9 +2,6 @@ defmodule AccountingSystemWeb.ShowXmlController do
   use AccountingSystemWeb, :controller
   alias Phoenix.LiveView
 
-  def index(conn, params) do
-    LiveView.Controller.live_render(conn, AccountingSystemWeb.ShowXmlLiveView, session: %{"xml_id" => params["xml_id"]})
-  end
   def show(conn, params) do
     file_name = params["xml_name"] |> String.replace(" ", "")
     data = AccountingSystem.AuxiliaryHandler.get_xml_file_to_alexandria(params["xml_id"])
--- apps/helsinki_web/lib/helsinki_web/router.ex
+++ apps/helsinki_web/lib/helsinki_web/router.ex
@@ -30,7 +30,6 @@ defmodule AccountingSystemWeb.Router do
     get "/listconfiguration", ListConfigurationController, :index
     get "/download_template", DownloadController, :index
     get "/show_xml/:xml_id/:xml_name", ShowXmlController, :show
-    #get "/show_xml.xml", ShowXmlController, :index
     # get "/submenu", SubMenuController, :sub_menu
     resources "/accounts", AccountController
     resources "/structures", StructureController
