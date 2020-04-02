721103aebfdb5c50c1bdce128b60c163f2525268
pedrogmucino <enriquegmucino@gmail.com>
2020-04-02 15:46:41 -0600

    Versión compilación elixir 1.9

--- apps/helsinki_web/mix.exs
+++ apps/helsinki_web/mix.exs
@@ -9,7 +9,7 @@ defmodule AccountingSystemWeb.MixProject do
       config_path: "../../config/config.exs",
       deps_path: "../../deps",
       lockfile: "../../mix.lock",
-      elixir: "~> 1.10.2",
+      elixir: "~> 1.9",
       elixirc_paths: elixirc_paths(Mix.env()),
       compilers: [:phoenix, :gettext] ++ Mix.compilers(),
       start_permanent: Mix.env() == :prod,
