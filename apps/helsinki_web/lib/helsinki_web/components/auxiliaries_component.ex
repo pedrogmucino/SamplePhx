aa969412dc11f8fd344bf046c4df95251128ed58
Héctor López <hector.lopez@DSA-DEV2-10>
2020-04-03 17:47:57 -0600

    trabajando en consulta de aux con un mes de prueba

--- apps/helsinki_web/lib/helsinki_web/components/auxiliaries_component.ex
+++ apps/helsinki_web/lib/helsinki_web/components/auxiliaries_component.ex
@@ -15,7 +15,7 @@ defmodule AccountingSystemWeb.AuxiliariesComponent do
 
   def render(assigns) do
     ~L"""
-      <div class="mt-16 ml-16 h-hoch-93 w-full bg-white">
+      <div class="mt-16 ml-16 h-auto w-full bg-white">
 
         <div class="inline-block bg-blue-700 text-white px-6 py-4 w-full">
           <button class="ml-mar-380 -mr-2 -mt-4 text-white font-bold rounded shadow">
@@ -25,12 +25,12 @@ defmodule AccountingSystemWeb.AuxiliariesComponent do
           </button>
         </div>
 
-      <div class="m-4 mr-4 mt-4 mb-0 border-t-2 border-gray-300"></div>
+      <div class="m-4 mr-4 mt-4 mb-4 border-t-2 border-gray-300"></div>
         <table class="table-auto">
           <thead>
             <tr>
               <th class="w-48 px-4 py-2">Tipo Póliza</th>
-              <th class="w-40 px-4 py-2">Numero Póliza</th>
+              <th class="w-40 px-4 py-2">Número Póliza</th>
               <th class="w-48 px-4 py-2">Fecha</th>
               <th class="w-64 px-4 py-2">Cuenta Detalle</th>
               <th class="w-40 px-4 py-2">Caja</th>
@@ -39,26 +39,26 @@ defmodule AccountingSystemWeb.AuxiliariesComponent do
             </tr>
           </thead>
         </table>
-        <div class="ml-4 mr-4 border-solid border-2 border-gray-300 p-4 rounded">
+        <div class="ml-4 mr-4 mb-4 border-solid border-2 border-gray-300 p-4 rounded">
           <%= for item <- @list_auxiliaries do %>
             <div class="py-1">
-              <label><b><%= item.nombre %></b></label>
+              <label><b><%= item.name %></b></label>
               <div class="bg-gray-300 border rounded relative">
-                <%= for item2 <- item.auxiliaries do %>
+                <%= for item2 <- item.details do %>
                   <div>
-                    <div class="inline-flex w-48 px-2 py-2"><%= item2.tipo %></div>
-                    <div class="inline-flex w-40 px-2 py-2"><%= item2.numero_poliza %></div>
-                    <div class="inline-flex w-48 px-2 py-2"><%= item2.fecha %></div>
-                    <div class="inline-flex w-64 px-2 py-2"><%= item2.cuenta_detalle %></div>
-                    <div class="inline-flex w-40 px-2 py-2 justify-end"><%= item2.caja %></div>
-                    <div class="inline-flex w-40 px-2 py-2 justify-end"><%= item2.abono %></div>
-                    <div class="inline-flex w-80 px-2 py-2"><%= item2.concepto %></div>
+                    <div class="inline-flex w-48 px-2 py-2"><%= item2.policy_type %></div>
+                    <div class="inline-flex w-40 px-2 py-2"><%= item2.number %></div>
+                    <div class="inline-flex w-48 px-2 py-2"><%= item2.date %></div>
+                    <div class="inline-flex w-64 px-2 py-2"><%= item.code %></div>
+                    <div class="inline-flex w-40 px-2 py-2 justify-end"><%= if item2.auxiliary_type == "D", do: item2.amount %></div>
+                    <div class="inline-flex w-40 px-2 py-2 justify-end"><%= if item2.auxiliary_type == "H", do: item2.amount %></div>
+                    <div class="inline-flex w-80 px-2 py-2"><%= item2.concept %></div>
                   </div>
                 <% end %>
                 <div>
-                  <div class="inline-flex w-200 px-4 py-2">1</div>
-                  <div class="inline-flex w-40 px-4 py-2 justify-end ml-5"><%= item.total_caja %></div>
-                  <div class="inline-flex w-40 px-4 py-2 justify-end"><%= item.total_abono %></div>
+                  <div class="inline-flex w-200 px-4 py-2"></div>
+                  <div class="inline-flex w-40 px-4 py-2 justify-end ml-5"><%= item.debe %></div>
+                  <div class="inline-flex w-40 px-4 py-2 justify-end"><%= item.haber %></div>
                 </div>
               </div>
             </div>
@@ -68,58 +68,11 @@ defmodule AccountingSystemWeb.AuxiliariesComponent do
     """
   end
 
-  defp get_auxiliaries(), do:
-  [
-    %{
-      nombre: "Acredor 1",
-      total_caja: "50.00",
-      total_abono: "50.00",
-      auxiliaries: [
-        %{
-          tipo: "Gastos",
-          numero_poliza: "1",
-          fecha: "01/28/2020",
-          cuenta_detalle: "2-001-002-001",
-          caja: "",
-          abono: "50.00",
-          concepto: "Sueldos y salarios por pagar"
-        },
-        %{
-          tipo: "Egresos",
-          numero_poliza: "1",
-          fecha: "01/28/2020",
-          cuenta_detalle: "2-001-002-001",
-          caja: "50.00",
-          abono: "",
-          concepto: "Pago de sueldos y salarios"
-        }
-      ]
-    },
-    %{
-      nombre: "Bancomer",
-      total_caja: "50.00",
-      total_abono: "50.00",
-      auxiliaries: [
-        %{
-          tipo: "Egresos",
-          numero_poliza: "1",
-          fecha: "01/28/2020",
-          cuenta_detalle: "1-001-002-001",
-          caja: "",
-          abono: "50.00",
-          concepto: "Sueldos y salarios por pagar"
-        },
-        %{
-          tipo: "Ingresos",
-          numero_poliza: "1",
-          fecha: "01/28/2020",
-          cuenta_detalle: "1-001-002-002",
-          caja: "50.00",
-          abono: "",
-          concepto: "Cobranza Cliente"
-        }
-      ]
-    }
-  ]
+  defp get_auxiliaries() do
+    start_date = Date.from_iso8601!("2020-03-01")
+    end_date = Date.from_iso8601!("2020-04-03")
+    AccountingSystem.AuxiliaryHandler.get_aux_report(start_date, end_date)
+    |> IO.inspect(label: "true list")
+  end
 
 end
